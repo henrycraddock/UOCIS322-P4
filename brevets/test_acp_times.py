4e433f8b8029619bb2451a22721eec60fcd1d83d
Henry Craddock <Henry@Henrys-MacBook-Pro.local>
2021-05-15 16:15:29 -0700

    Updated README.md

---
+++ brevets/test_acp_times.py
@@ -0,0 +1,12 @@
+"""
+Nose tests for acp_times.py
+"""
+
+import acp_times
+
+import nose
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s',
+                    level=logging.WARNING)
+log = logging.getLogger(__name__)
+
